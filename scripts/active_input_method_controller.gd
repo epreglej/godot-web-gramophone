334ea28d330ae807ae3906127f4b6c0aa0c1d6da
epreglej <eugen.preglej@gmail.com>
2026-01-02 19:24:30 +0100

    Migrated to Hand Pose Detector way of checking for active hand tracking.

--- scripts/active_input_method_controller.gd
+++ scripts/active_input_method_controller.gd
@@ -3,37 +3,47 @@ extends XROrigin3D
 @export var left_joystick_controller: XRController3D
 @export var left_hand_controller: XRController3D
 @export var left_hand_origin: XRNode3D
+@export var hand_pose_detector: HandPoseDetector
 @export var label: Label3D
 
 func _process(_delta):
-	var tracker = XRServer.get_tracker("/user/hand_tracker/left")
-	var using_hands := false
+	if not hand_pose_detector or not hand_pose_detector.hand_tracker:
+		return
 
-	if tracker:
-		# Only use hands if real tracking data is present
-		using_hands = tracker.has_tracking_data and tracker.hand_tracking_source == XRHandTracker.HAND_TRACKING_SOURCE_UNOBSTRUCTED
+	# Check if the palm is tracked
+	var flags := hand_pose_detector.hand_tracker.get_hand_joint_flags(XRHandTracker.HAND_JOINT_PALM)
+	var using_hands: bool = false
+	if (flags & XRHandTracker.HAND_JOINT_FLAG_POSITION_TRACKED) != 0 \
+	   and (flags & XRHandTracker.HAND_JOINT_FLAG_ORIENTATION_TRACKED) != 0:
+		using_hands = true
+
+	# Optional: you can also check if a pose is currently active
+	# using_hands = hand_pose_detector._current_pose != null
 
 	var using_controller := not using_hands
 
-	# for debug
+	# Debug print
 	print("hands:", using_hands, " controller:", using_controller)
-	if label:
-		label.text = "hands:" + str(using_hands) + " controller:" + str(using_controller)
 
-	# Controller
+	# Update label
+	if label:
+		label.text = "hands: " + str(using_hands) + " controller: " + str(using_controller)
+
+	# Controller visibility & processing
 	left_joystick_controller.visible = using_controller
 	left_joystick_controller.process_mode = (
 		Node.PROCESS_MODE_INHERIT if using_controller
 		else Node.PROCESS_MODE_DISABLED
 	)
 
-	# Hand pose mode
+	# Hand pose controller visibility & processing
 	left_hand_controller.visible = using_hands
 	left_hand_controller.process_mode = (
 		Node.PROCESS_MODE_INHERIT if using_hands
 		else Node.PROCESS_MODE_DISABLED
 	)
 
+	# Hand origin visibility & processing
 	left_hand_origin.visible = using_hands
 	left_hand_origin.process_mode = (
 		Node.PROCESS_MODE_INHERIT if using_hands
