5a9f22f1eaea5190b0d19cef5b78f9017a75d4d1
epreglej <eugen.preglej@gmail.com>
2026-01-04 17:53:08 +0100

    Added lid hinge movement.

--- scripts/vinyl_snap_zone.gd
+++ scripts/vinyl_snap_zone.gd
@@ -2,20 +2,16 @@
 extends XRToolsSnapZone
 class_name VinylSnapZone
 
-@export var label: Label3D
 
-var vinyl: Vinyl = null
-
-
-func pick_up_object(target: Node3D) -> void:
-	if not target is Vinyl:
-		return
-	
-	vinyl = target as Vinyl
-
-	# Let XRTools handle position snapping
-	super.pick_up_object(target)
-
-	# Optional debug label showing current song title
-	if label and is_instance_valid(vinyl):
-		label.text = vinyl.song.title
+#func pick_up_object(target: Node3D) -> void:
+	#if not target is Vinyl:
+		#return
+	#
+	#vinyl = target as Vinyl
+#
+	## Let XRTools handle position snapping
+	#super.pick_up_object(target)
+#
+	### Optional debug label showing current song title
+	##if label and is_instance_valid(vinyl):
+		##label.text = vinyl.song.title
